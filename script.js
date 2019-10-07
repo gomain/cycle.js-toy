0bbe41724f1a5e5317bf8041cdcf3662dffa57b0
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-07 18:38:43 +0700

    cycle of source & sinks, recusive dom builder

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
+import xs from 'xstream';
+import fromEvent from 'xstream/extra/fromEvent';
 
 //Logic
-function main () {
+function main (sources) {
+  const click$ = sources.DOM;
   return {
-    DOM: xs.periodic(1000)
-      .fold(acc => acc+1,0)
-      .map(i => `Seconds elapsed: ${i}`),
+    DOM: click$.map(() => xs
+		    .periodic(1000)
+		    .fold(acc => acc+1,0))
+      .flatten()
+      .map(i => ([
+        'simple text',
+        {
+          tagName: 'h1',
+          children: [ {
+            tagName: 'span',
+            children: [ `Seconds elapsed: ${i}` ]
+          }]
+        }])),
     log: xs.periodic(2000)
-      .fold(acc => acc+1,0)
+      .fold(acc => acc+2,0)
+      .map(i => xs.create().startWith(i))
+      .flatten()
   };
 }
 
-//Effect
-function domDriver(text$) {
-  text$.subscribe({
-    next: str => {
-      document.querySelector('#app').textContent = str;
+//Effect(s)
+function domDriver(dom$) {
+  //Write
+  dom$.subscribe({
+    next: children => {
+      const root =document.querySelector('#app');
+      root.innerHTML = '';
+      createChildren(root,children);
     }
   });
+
+  function createChildren(parent,childrenSpecs) {
+    if (typeof childrenSpecs === 'string') {
+      parent.append(childrenSpecs);
+    } else if (childrenSpecs instanceof Array) {
+      childrenSpecs.forEach(childSpecs => {
+        createChildren(parent,childSpecs);
+      });
+    } else if (childrenSpecs instanceof Object) {
+      const childElement = document.createElement(childrenSpecs.tagName);
+      createChildren(childElement,childrenSpecs.children);
+      parent.append(childElement);
+    }
+  }
+  //Read
+  const domSource = fromEvent(document, 'click');
+  return domSource;
 }
 
 function logDriver(msg$) {
@@ -28,11 +63,28 @@ function logDriver(msg$) {
 }
 
 function run(mainFn, drivers) {
-  const sinks = mainFn();
-  Object.entries(drivers).forEach(([key,driver]) => {
-    if (sinks[key]) {
-      driver(sinks[key]);
-    }
+  const sources = {};
+  const fakeSinks = {};
+
+  Object.entries(drivers).forEach(([driverKey,driver]) => {
+    const fakeSink = xs.create();
+    fakeSinks[driverKey] = fakeSink;
+    const source = driver(fakeSink);
+    sources[driverKey] = source;
+  });
+  const sinks = mainFn(sources);
+  Object.entries(sinks).forEach(([sinkKey,sink]) => {
+    Object.entries(fakeSinks).forEach(([fakeSinkKey,fakeSink]) => {
+      if (sinkKey === fakeSinkKey) {
+        try {
+	  fakeSink.imitate(sink);
+        } catch (e) {
+          console.log(fakeSink);
+          console.log(sink);
+          console.log(e);
+        }
+      }
+    });
   });
 }
 
